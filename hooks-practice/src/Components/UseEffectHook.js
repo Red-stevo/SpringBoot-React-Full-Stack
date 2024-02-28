2f0628d3d039cd38b21d1f123b4e60003cbebdbf
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-02-28 18:49:09 +0300

    conditional rendering

--- hooks-practice/src/Components/UseEffectHook.js
+++ hooks-practice/src/Components/UseEffectHook.js
@@ -1,10 +1,18 @@
+import TextInput from "./TextInput";
+import ChoiceInput from "./ChoiceInput";
+
 const UseEffectHook = () =>{
 
-    const fieldNames = ["text-input", "text-input", "choice-input"]
+    const fieldNames = ["choice-input","text-input", "choice-input", "text-input", "choice-input"]
 
     const form = fieldNames.map(
         (field, key) => {
-
+            return(
+                <div>
+                    {field === "text-input" && <TextInput />}
+                    {field === "choice-input" && <ChoiceInput />}
+                </div>
+            )
         }
     )
 
