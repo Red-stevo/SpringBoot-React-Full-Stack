7dcf1a75835cf8aacd593db30ce917581772e90d
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-02-28 18:42:27 +0300

    conditional rendering

---
+++ hooks-practice/src/Components/UseEffectHook.js
@@ -0,0 +1,17 @@
+const UseEffectHook = () =>{
+
+    const fieldNames = ["text-input", "text-input", "choice-input"]
+
+    const form = fieldNames.map(
+        (field, key) => {
+
+        }
+    )
+
+    return(<>
+            {form}
+        </>
+    );
+}
+
+export default UseEffectHook;
